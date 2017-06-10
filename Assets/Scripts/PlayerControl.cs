a53875090e8a98d8c990c556959aceeb22f47906
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-06-10 14:34:27 -0400

    Added count & win text

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour {
 
 	public float speed;
+	public Text countText;
+	public Text winText;
 
 	private Rigidbody rb;
+	private int count;
 
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
 	}
 
 	// Physics
@@ -29,6 +36,17 @@ public class PlayerControl : MonoBehaviour {
 		if(other.gameObject.CompareTag("PickUp"))
 			{
 				other.gameObject.SetActive(false);
+				count++;
+				SetCountText ();
 			}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12)
+		{
+			winText.text = "You Win!";
+		}
 	}
 }
